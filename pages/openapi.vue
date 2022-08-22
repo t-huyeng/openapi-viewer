24415ec92d61311dd73492f2238179ec98d253c7
Huyeng <huyeng@iib.tu-darmstadt.de>
2022-08-22 22:21:53 +0200

    added autocomplete to select openapi

--- pages/openapi.vue
+++ pages/openapi.vue
@@ -1,12 +1,42 @@
 <template>
-  <Swagger-Ui :spec="spec"></Swagger-Ui>
+  <div>
+    <v-autocomplete
+      return-object
+      v-model="selectedAPI"
+      :items="items"
+      item-text="name"
+      @change="update()"
+      menu-props="closeOnContentClick"
+      clearable
+    ></v-autocomplete>
+
+    <Swagger-Ui v-if="show" :spec="spec"></Swagger-Ui>
+  </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    spec: require("~/assets/openapi/openapi.json"),
+    items: [
+      {
+        name: "Pegel-online API",
+        spec: require("~/assets/openapi/openapi.json"),
+      },
+    ],
+    spec: "",
+    selectedAPI: null,
+    show: false,
   }),
+
+  methods: {
+    update() {
+      this.show = false;
+      if (this.selectedAPI) {
+        this.spec = this.selectedAPI.spec;
+        this.show = true;
+      }
+    },
+  },
 };
 </script>
 

6f21d1b501230d43f305e144acb3082adad019ed
Huyeng <huyeng@iib.tu-darmstadt.de>
2022-08-22 21:01:06 +0200

    :tada: initial commit

---
+++ components/swagger-ui.vue
@@ -0,0 +1,27 @@
+ <template>
+  <div id="swagger" class="swagger"></div>
+</template>
+
+ <script>
+import SwaggerUIBundle from "~/assets/swagger/swagger-ui-bundle.js";
+export default {
+  name: "Swagger-UI",
+
+  props: {
+    spec: {
+      required: true,
+    },
+  },
+
+  mounted() {
+    SwaggerUIBundle({
+      spec: this.spec,
+      dom_id: "#swagger",
+    });
+  },
+};
+</script>
+
+
+<style>
+</style>
---
+++ pages/openapi.vue
@@ -0,0 +1,14 @@
+<template>
+  <Swagger-Ui :spec="spec"></Swagger-Ui>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    spec: require("~/assets/openapi/openapi.json"),
+  }),
+};
+</script>
+
+<style>
+</style>

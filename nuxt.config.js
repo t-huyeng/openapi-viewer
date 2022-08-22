cd538c24a27d45a8ab30110b3b7aabf355c5b091
Huyeng <huyeng@iib.tu-darmstadt.de>
2022-08-22 21:08:57 +0200

    update base-router for github-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/nuxtjs-gh-pages-openapi/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
